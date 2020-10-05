932d633b5392320fb96c3c66eac31a49c5315aa9
Dmytro Zikratyi <shooterdimon@gmail.com>
2020-10-05 16:59:49 +0300

    Own 5-15 changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,20 +1,22 @@
 #include "calculator.h"
 
+// + function
 int Calculator::Add (double a, double b)
 {
-<<<<<<< HEAD
-    return a + b;
-=======
-	return a + b + 0.5;
->>>>>>> 4ad40a3... fix truncation error
+	return a + b;
 }
-
+// - function
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
-
+// * function 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
+}
+// / function
+int Calculator::Div (double a, double b)
+{
+    return a / b;
 }
